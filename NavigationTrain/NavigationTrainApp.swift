e124099a6bfac024462a85f1bf50590d612b66ad
danyaDerdan <110595066+danyaDerdan@users.noreply.github.com>
2024-11-04 09:39:47 +0300

    Initial Commit

---
+++ NavigationTrain/NavigationTrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationTrainApp.swift
+//  NavigationTrain
+//
+//  Created by Данил Толстиков on 04.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationTrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NavigationTrainTests/NavigationTrainTests.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationTrainTests.swift
+//  NavigationTrainTests
+//
+//  Created by Данил Толстиков on 04.11.2024.
+//
+
+import Testing
+@testable import NavigationTrain
+
+struct NavigationTrainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
